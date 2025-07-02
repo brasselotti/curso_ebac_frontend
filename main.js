1327c74bf83e16db46fd18c7c225a8c7f72e35be
brasselotti <brasselottif@gmail.com>
2025-07-02 13:52:56 -0300

    checkpoint - adicionando tarefas

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    alert("Seja bem vindo!")
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        let tarefa = $('#tarefa').val();
+        if (tarefa) {
+            tarefa = tarefa.charAt(0).toUpperCase() + tarefa.slice(1).toLowerCase() + ';';
+        }
+        let incluirTarefa = $('<li></li>').text(tarefa);
+
+        $(incluirTarefa).appendTo('ul')
+        $('#container-list').fadeIn(500)
+        $('#tarefa').val('');
+    })
 })
